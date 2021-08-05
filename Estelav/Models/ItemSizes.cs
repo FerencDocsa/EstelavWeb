b8fe16d86b16b2fdf7e5296bb65a9d81607e403a
Ferenc Docsa <dof9jh@bosch.com>
2021-08-05 15:52:06 +0200

    Admin Panel Manager

---
+++ Estelav/Models/ItemSizes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Estelav.Models
+{
+    public partial class ItemSizes
+    {
+        public int ItemId { get; set; }
+        public string Size { get; set; }
+        public int Amount { get; set; }
+
+        public virtual Items Item { get; set; }
+    }
+}
--- Estelav/Pages/Panel/ItemsManager.cshtml.cs
+++ Estelav/Pages/Panel/ItemsManager.cshtml.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Estelav.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +13,69 @@ namespace Estelav.Pages.Panel
 {
     public class ItemsManagerModel : PageModel
     {
+        private readonly EstelavContext _context;
+
+        public List<Categories> categoriesList { get; set; }
+
+        public class UploadModel
+        {
+            [Required]
+            public string Name { get; set; }
+
+            [Required]
+            public int CatergoryId { get; set; }
+
+            [Required]
+            IFormFile mainPhoto { get; set; }
+
+            [Required]
+            IFormFile[] descrPhotos { get; set; }
+
+            List<string> sizes { get; set; }
+
+            public int Price { get; set; }
+
+
+
+            public string Description { get; set; }
+
+            public string MainImage { get; set; }
+
+            public List<string> ImageList { get; set; }
+        }
+
+        public ItemsManagerModel(EstelavContext context)
+        {
+            _context = context;
+        }
+
         public void OnGet()
         {
+            categoriesList = _context.Categories.ToList();
         }
+
+        public IActionResult OnPost(IFormFile[] photos, Items? product = null)
+        {
+            if (photos == null || photos.Length == 0)
+            {
+                return Content("File(s) not selected");
+            }
+            else
+            {
+                //product.Photos = new List<string>();
+                foreach (IFormFile photo in photos)
+                {
+                    Guid id = Guid.NewGuid();
+
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/merch", id + "_" + photo.FileName);
+                    var stream = new FileStream(path, FileMode.Create);
+                    photo.CopyToAsync(stream);
+                    //product.Photos.Add(photo.FileName);
+                }
+            }
+            //ViewBag.product = product;
+            return new OkResult();
+        }
+
     }
 }
